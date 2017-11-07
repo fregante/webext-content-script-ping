4b8ac2c050dcc7e1e1f0d133b079d246410a5c5b
Federico Brigante <github@bfred.it>
2017-11-07 12:47:09 +0800

    Add temporary logging

--- webext-content-script-ping.js
+++ webext-content-script-ping.js
@@ -2,12 +2,19 @@
 
 function pingContentScript(tab) {
 	return new Promise((resolve, reject) => {
-		setTimeout(reject, 300);
+		console.log('webext-content-script-ping: will ping');
+		console.log(tab);
+		setTimeout(() => {
+			console.log('webext-content-script-ping: timeout');
+			console.log(tab);
+			reject();
+		}, 300);
 		chrome.tabs.sendMessage(tab.id || tab, chrome.runtime.id, {
 			// Only the main frame is necessary;
 			// if that isn't loaded, no other iframe is
 			frameId: 0
 		}, response => {
+			console.log('webext-content-script-ping: got ping back: ', response);
 			if (response === chrome.runtime.id) {
 				resolve();
 			} else {
@@ -20,6 +27,7 @@ function pingContentScript(tab) {
 if (!chrome.runtime.getBackground) {
 	// Respond to pings
 	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		console.log('webext-content-script-ping: got ping: ', request, chrome.runtime.id);
 		if (request === chrome.runtime.id) {
 			sendResponse(chrome.runtime.id);
 		}
